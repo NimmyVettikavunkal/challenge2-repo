8f3c57ac458f6604779737b8c19b7f030a3f67b6
Nimmy <nimmyaugustin@gmail.com>
2023-10-18 17:54:51 +0000

    controller class added to project

---
+++ src/main/java/com/example/challengetwodemo/controller/PersonController.java
@@ -0,0 +1,21 @@
+package com.example.challengetwodemo.controller;
+
+import com.example.challengetwodemo.model.Person;
+import com.example.challengetwodemo.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PersonController {
+    @Autowired
+    private PersonRepository personRepository;
+
+    @GetMapping("/person")
+    public List<Person> getAllPersons(){
+
+        return personRepository.findAll();
+    }
+}
