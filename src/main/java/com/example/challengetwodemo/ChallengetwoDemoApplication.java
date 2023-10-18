f78ad5b6bda17d60372af22ba17376273d646a70
Nimmy <nimmyaugustin@gmail.com>
2023-10-18 17:41:33 +0000

    Initial commit

---
+++ src/main/java/com/example/challengetwodemo/ChallengetwoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.challengetwodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengetwoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengetwoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/challengetwodemo/ChallengetwoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challengetwodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengetwoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
