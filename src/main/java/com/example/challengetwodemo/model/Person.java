8419adb0b9e2d4f38278a54909b407d1c45b8915
Nimmy <nimmyaugustin@gmail.com>
2023-10-18 17:47:03 +0000

    model class generated

---
+++ src/main/java/com/example/challengetwodemo/model/Person.java
@@ -0,0 +1,57 @@
+package com.example.challengetwodemo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int age;
+    private String name;
+    private String address;
+
+    public Person() {
+    }
+
+    public Person(int age, String name, String address) {
+        this.age = age;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
